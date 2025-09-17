cfcc686382f78cdb6fea03ff7362cbaa3c3e2ebd
rafaelffz <devrafaelffz@gmail.com>
2025-09-16 23:08:01 -0300

    initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-4xl">Hello World!</div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/image", "@nuxt/ui", "@nuxt/fonts"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
