d2caa5deb766f74fbd1b4214d077c8c0d022eb8d
rafaelffz <devrafaelffz@gmail.com>
2025-09-18 02:04:57 -0300

    feat: add landing page feat: add login page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,50 @@
-<script setup lang="ts">
-
+<script lang="ts" setup>
+const router = useRouter();
 </script>
 
 <template>
-  <div class="text-4xl">Hello World!</div>
+  <div class="h-dvh w-full relative font-Inter">
+    <div
+      class="bg-[url('@/assets/images/bg-login.jpg')] h-dvh w-full bg-cover bg-center brightness-[30%] absolute"
+    ></div>
+
+    <div class="container mx-auto px-4 relative z-10 flex flex-col items-center h-full">
+      <nav
+        class="border border-zinc-500 bg-zinc-800/90 w-full max-w-3xl rounded-xl p-3 mt-4 flex items-center justify-between"
+      >
+        <div class="flex items-center gap-2">
+          <img src="@/assets/images/icon.svg" alt="Logo Hora do Banho" class="size-10" />
+          <p class="font-Poppins text-xl font-semibold">Hora do Banho</p>
+        </div>
+
+        <div>
+          <UButton
+            variant="solid"
+            color="neutral"
+            size="lg"
+            class="cursor-pointer w-24 flex items-center text-base justify-center"
+            @click="router.push('/login')"
+          >
+            Entrar
+          </UButton>
+        </div>
+      </nav>
+
+      <div class="text-center text-white max-w-3xl h-full flex flex-col items-center justify-center">
+        <h1 class="text-3xl md:text-4xl font-bold text-shadow-sm mb-6 text-shadow-zinc-700 leading-tight">
+          Transforme a gestão do seu <span class="bg-primary px-1">banho e tosa</span> de forma inteligente
+        </h1>
+
+        <p class="text-lg md:text-2xl text-zinc-200 text-shadow-sm text-shadow-zinc-700 leading-tight">
+          Aumente a produtividade do seu negócio com um sistema completo para agendamento e gestão de serviços de banho
+          e tosa para pets.
+        </p>
+
+        <UButton size="xl" class="text-white text-base px-6 py-3 mt-12 cursor-pointer" @click="router.push('/login')">
+          <Icon name="tabler:rocket" class="size-6" />
+          Começar Agora
+        </UButton>
+      </div>
+    </div>
+  </div>
 </template>
---
+++ app/pages/login.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+const router = useRouter();
+</script>
+
+<template>
+  <div class="h-dvh w-full font-Inter">
+    <div class="bg-[url('@/assets/images/bg-login.jpg')] h-dvh w-full bg-contain brightness-[30%] absolute"></div>
+
+    <div class="flex flex-col size-full justify-center items-center px-8 gap-8">
+      <div class="w-full max-w-xl rounded-xl bg-zinc-100 brightness-90 z-10 p-4 space-y-8">
+        <div class="flex items-center justify-start w-full">
+          <img src="@/assets/images/icon.svg" alt="Logo Hora do Banho" class="size-12" />
+        </div>
+
+        <div class="p-2 space-y-8">
+          <div class="text-black text-3xl font-bold text-center">
+            <div class="space-y-6">
+              <h1 class="text-balance text-2xl md:text-3xl text-zinc-800">
+                Entre na <span class="text-primary">Hora do Banho</span> e agende os banhos dos pets com facilidade!
+              </h1>
+              <p class="mt-2 text-sm md:text-base font-normal text-balance text-zinc-600">
+                Não perca tempo, faça login com sua conta Google e aproveite todos os benefícios da nossa plataforma.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <UButton variant="subtle" color="primary" class="cursor-pointer" block @click="authStore.signIn">
+              <Icon name="devicon:google" />
+              Entrar com o Google
+            </UButton>
+          </div>
+        </div>
+      </div>
+
+      <UButton variant="solid" class="z-10 cursor-pointer text-white" @click="router.push('/')">
+        <Icon name="tabler:arrow-left" />
+        Voltar para Página Inicial
+      </UButton>
+    </div>
+  </div>
+</template>
