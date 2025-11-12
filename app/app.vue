a18cdf8612d3449fc66806416d5e129ac41156df
rafaelffz <devrafaelffz@gmail.com>
2025-11-12 00:22:04 -0300

    feat(toast): change position

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { pt_br } from "@nuxt/ui/locale"
+import type { ToasterProps } from "@nuxt/ui"
+
+const toaster = { position: "top-left" } as ToasterProps
 </script>
 
 <template>
-  <UApp :locale="pt_br">
+  <UApp :locale="pt_br" :toaster="toaster">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
